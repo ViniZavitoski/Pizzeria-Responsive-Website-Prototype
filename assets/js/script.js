69c3e938eec5acebc6807c1a30140e61022a9e88
Vinicius Zavitoski <odeayotunde@gmail.com>
2024-02-08 03:06:49 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+let carrinho = document.querySelector(".carrinho");
+document.querySelector("#cart").onclick = () => {
+    carrinho.classList.toggle("active");
+
+    login.classList.remove("active");
+
+}
+
+let login = document.querySelector(".login-form");
+document.querySelector("#login").onclick = () => {
+    login.classList.toggle("active");
+
+    carrinho.classList.remove("active");
+
+}
+
+const swiper = new Swiper(".home-slider", {
+
+    autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    gapCursor: true,
+    loop: true,
+    centeredSlides: true,
+    navigation: {
+        nextE1: '.swiper-button-next',
+        prevE1: '.swiper-button-prev',
+    },
+})
