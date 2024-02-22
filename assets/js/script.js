59590ea5b5b711f7031056ed49c76f39ce229ee2
Vinicius Zavitoski <odeayotunde@gmail.com>
2024-02-22 20:02:43 -0300

    7 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,22 @@ document.querySelector("#cart").onclick = () => {
 
     login.classList.remove("active");
 
-}
+};
 
 let login = document.querySelector(".login-form");
 document.querySelector("#login").onclick = () => {
     login.classList.toggle("active");
 
     carrinho.classList.remove("active");
-}
+};
 
 window.onscroll = () => {
     carrinho.classList.remove("active");
-    
-    login.classList.remove("active");
-}
 
-const swiper = new Swiper(".home-slider", {
+    login.classList.remove("active");
+};
+
+var swiper = new Swiper(".home-slider", {
 
     autoplay:{
         delay:2500,
@@ -32,4 +32,28 @@ const swiper = new Swiper(".home-slider", {
         nextE1: '.swiper-button-next',
         prevE1: '.swiper-button-prev',
     },
-})
+});
+
+let verModalCorpo = document.querySelector(".menu-modal-container");
+let verModalBox = verModalCorpo.querySelector(".menu-modal");
+
+document.querySelectorAll(".menu .box").forEach(menu => {
+    menu.onclick = () => {
+        verModalCorpo.style.display = 'flex';
+        let nome = menu.getAttribute('data-name');
+
+        verModalBox.forEach(visualizar => {
+            let chamada = visualizar.getAttribute('data-target');
+            if(nome == chamada){
+                visualizar.classList.add('active');
+            };
+        });
+    };
+});
+
+verModalCorpo.querySelector("#fechar").onclick = () => {
+    verModalCorpo.style.display = 'none';
+    verModalBox.forEach(fechar=> {
+        fechar.classList.remove('active');
+    });
+};;
